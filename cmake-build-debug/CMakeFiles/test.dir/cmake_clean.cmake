c1b484a67f13d7fc7bca63f6795b6f52567acf9d
Sharma <csharma22>
2019-01-05 22:42:40 +0000

    fix the template  and make it easy for c++

---
+++ cmake-build-debug/CMakeFiles/test.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test.dir/tasks/Task.cpp.o"
+  "CMakeFiles/test.dir/tasks/TaskA.cpp.o"
+  "test.pdb"
+  "test.exe"
+  "libtest.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
